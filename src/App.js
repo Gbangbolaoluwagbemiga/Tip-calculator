0715b22ff7c3a904b0c58b36eff307f8afbf8d30
Oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-07-15 13:24:44 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ function FriendServiceRating({ friend, onRating }) {
     <div>
       <p style={{ display: "inline" }}>How did your friend like the service?</p>
       <select value={friend} onChange={(e) => onRating(e)}>
-        <option value="5"> Dissatisfied (0%) </option>
+        <option value="0"> Dissatisfied (0%) </option>
         <option value="5"> Not bad (5%) </option>
         <option value="10">It was good (10%)</option>
         <option value="20">Absolutely amazing (20%)</option>
